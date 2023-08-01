0a40098782ceac40542d200070436a834f087c0c
stancecoke <hochsitzcola@web.de>
2023-08-01 20:55:00 +0200

    Bugfix in J-LCD protocol

--- Inc/config.h
+++ Inc/config.h
@@ -8,6 +8,8 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 #include "stdint.h"
+
+// System constants, don't touch!
 #define DISPLAY_TYPE_EBiCS (1<<5)                  // King-Meter 618U protocol (KM5s, EBS-LCD2, J-LCD, SW-LCD)
 #define DISPLAY_TYPE_KINGMETER_618U (1<<3)                  // King-Meter 618U protocol (KM5s, EBS-LCD2, J-LCD, SW-LCD)
 #define DISPLAY_TYPE_KINGMETER_901U (1<<4)                  // King-Meter 901U protocol (KM5s)
@@ -15,7 +17,10 @@
 #define DISPLAY_TYPE_BAFANG (1<<2)							// For 'Blaupunkt' Display of Prophete Entdecker
 #define DISPLAY_TYPE_KUNTENG (1<<1)							// For ASCII-Output in Debug mode
 #define DISPLAY_TYPE_DEBUG (1<<0)							// For ASCII-Output in Debug mode);
-
+#define EXTERNAL 1
+#define INTERNAL 0
+//----------------------------------------------------------------------
+// advanced setting, don't touch, if you don't know what you are doing!
 #define TRIGGER_OFFSET_ADC 50
 #define TRIGGER_DEFAULT 2020
 #define _T 2028
@@ -26,11 +31,25 @@
 #define RESISTANCE 40LL
 #define FLUX_LINKAGE 1200LL
 #define GAMMA 9LL
+//#define FAST_LOOP_LOG
+//#define DISABLE_DYNAMIC_ADC
+//#define INDIVIDUAL_MODES
+//#define SPEEDTHROTTLE
+#define SIXSTEPTHRESHOLD 20000
+#define SPEED_PLL 0 //1 for using PLL, 0 for angle extrapolation
+#define P_FACTOR_PLL 10
+#define I_FACTOR_PLL 10
+
+//----------------------------------------------------------------------
+//Battery bar settings for Kunteng and Bafang Display
 #define BATTERY_LEVEL_1 323000
 #define BATTERY_LEVEL_2 329000
 #define BATTERY_LEVEL_3 344000
 #define BATTERY_LEVEL_4 368000
 #define BATTERY_LEVEL_5 380000
+
+//----------------------------------------------------------------------
+//PI-control factor settings
 #define P_FACTOR_I_Q 50
 #define I_FACTOR_I_Q 2
 #define P_FACTOR_I_D 50
@@ -42,6 +61,10 @@
 #define RAMP_END 1600
 #define THROTTLE_OFFSET 700
 #define THROTTLE_MAX 2850
+#define THROTTLE_OVERRIDE
+
+//--------------------------------------------------------------------
+//Speed settings
 #define WHEEL_CIRCUMFERENCE 2200
 #define GEAR_RATIO 60 //dummy for testing
 #define SPEEDLIMIT 25
@@ -58,27 +81,26 @@
 #define REVERSE 1
 #define PUSHASSIST_CURRENT 300
 #define VOLTAGE_MIN 300
+
+//---------------------------------------------------------------------
+//torquesensor settings
+#define TS_COEF 2000
+#define TS_MODE
+//#define TQONAD1
+
+//---------------------------------------------------------------------
+//Display settings
+#define DISPLAY_TYPE DISPLAY_TYPE_KINGMETER_618U
+
+
+//---------------------------------------------------------------------
+//Regen settings
+
 #define REGEN_CURRENT 200
-//#define FAST_LOOP_LOG
-//#define DISABLE_DYNAMIC_ADC
-//#define INDIVIDUAL_MODES
-//#define SPEEDTHROTTLE
-#define THROTTLE_OVERRIDE
 #define REGEN_CURRENT_MAX 10000
-
-
-#define P_FACTOR_PLL 10
-#define I_FACTOR_PLL 10
-#define AUTODETECT 0
-#define SPEED_PLL 0 //1 for using PLL, 0 for angle extrapolation
-
-#define EXTERNAL 1
-#define INTERNAL 0
-#define SPEEDSOURCE INTERNAL
-#define SPEEDFILTER 1
-#define SIXSTEPTHRESHOLD 20000
-
-#define SPDSHFT 0
 //#define ADC_BRAKE
+
+//---------------------------------------------------------------------
+#define AUTODETECT 0
 
 #endif /* CONFIG_H_ */
