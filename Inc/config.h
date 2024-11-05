55abc6c86a774dca63d8c232607f746c3f2fa54e
stancecoke <hochsitzcola@web.de>
2024-11-05 21:15:06 +0100

    Add autodetct feature with EN06 5s protocol

--- Inc/config.h
+++ Inc/config.h
@@ -90,7 +90,7 @@
 
 //---------------------------------------------------------------------
 //Display settings
-#define DISPLAY_TYPE DISPLAY_TYPE_NO2
+#define DISPLAY_TYPE DISPLAY_TYPE_KINGMETER_901U
 
 //---------------------------------------------------------------------
 //Regen settings
