779fc4a01e15faeae590ea6d8853cc2c6c62779a
0xDB6 <>
2026-04-04 22:11:16 +0200

    Revert autodetect and display

--- Inc/config.h
+++ Inc/config.h
@@ -106,7 +106,7 @@
 
 //---------------------------------------------------------------------
 //Display settings
-#define DISPLAY_TYPE DISPLAY_TYPE_DEBUG
+#define DISPLAY_TYPE DISPLAY_TYPE_KINGMETER_901U
 
 //---------------------------------------------------------------------
 //Regen settings
@@ -116,6 +116,6 @@
 //#define ADC_BRAKE
 
 //---------------------------------------------------------------------
-#define AUTODETECT 1
+#define AUTODETECT 0
 
 #endif /* CONFIG_H_ */
