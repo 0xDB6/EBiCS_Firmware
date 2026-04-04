bac1010e4b2760df12ce8a4c8761ab69b2a762f8
0xDB6 <>
2026-04-04 22:05:16 +0200

    change PLL values

--- Inc/config.h
+++ Inc/config.h
@@ -45,9 +45,9 @@
 //#define INDIVIDUAL_MODES
 //#define SPEEDTHROTTLE
 #define SIXSTEPTHRESHOLD 20000
-#define SPEED_PLL 0 //1 for using PLL, 0 for angle extrapolation
-#define P_FACTOR_PLL 14
-#define I_FACTOR_PLL 14
+#define SPEED_PLL 1 //1 for using PLL, 0 for angle extrapolation
+#define P_FACTOR_PLL 8
+#define I_FACTOR_PLL 10
 
 //----------------------------------------------------------------------
 //Battery bar settings for Kunteng and Bafang Display
