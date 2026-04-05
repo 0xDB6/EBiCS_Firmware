92e93af92a26fa0364888ae0457e4f32932d77dd
0xDB6 <>
2026-04-05 15:24:16 +0200

    P_FACTOR_PLL 9

--- Inc/config.h
+++ Inc/config.h
@@ -46,7 +46,7 @@
 //#define SPEEDTHROTTLE
 #define SIXSTEPTHRESHOLD 20000
 #define SPEED_PLL 1 //1 for using PLL, 0 for angle extrapolation
-#define P_FACTOR_PLL 5
+#define P_FACTOR_PLL 9
 #define I_FACTOR_PLL 9
 
 //----------------------------------------------------------------------
